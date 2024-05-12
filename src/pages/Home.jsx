aa799c2ffaf00e8ba727f836858f3bacc74bd91c
Krunal Gupta <guptakrunal02@gmail.com>
2024-05-12 23:31:21 +0530

    Render the trending gifs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useEffect } from "react";
+import Gif from "../components/Gif";
+import { GifState } from "../Context/Context";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+function Home() {
+  const { gf, gifs, setGifs, filter } = GifState();
+
+  const fetchTrendingGIFs = async () => {
+    const { data } = await gf.trending({
+      limit: 20,
+      type: filter,
+      rating: "g",
+    });
+    setGifs(data);
+  };
+
+  useEffect(() => {
+    fetchTrendingGIFs();
+  }, [filter]);
+
+  return (
+    <div className="">
+      <img
+        src="/banner.gif"
+        alt="earth banner"
+        className="mt-2 rounded w-full"
+      />
+
+      <div className="columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-2">
+        {gifs.map((gif) => (
+          <Gif gif={gif} key={gif} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default Home;
