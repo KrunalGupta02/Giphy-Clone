f2728776ddf2a98499310ce8e8358cf93e6f9aed
Krunal Gupta <guptakrunal02@gmail.com>
2024-05-12 18:29:15 +0530

    Add the basic header and create the files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppLayout from "./layouts/app-layout";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Search from "./pages/Search";
+import SingleGif from "./pages/Single-gif";
+import Favourite from "./pages/Favourite";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    // errorElement
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/:category",
+        element: <Category />,
+      },
+      {
+        path: "/search/:query",
+        element: <Search />,
+      },
+      {
+        path: "/:type/:slug",
+        element: <SingleGif />,
+      },
+      {
+        path: "/favourite",
+        element: <Favourite />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
