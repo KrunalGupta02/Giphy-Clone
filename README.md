40afd55fa20f108c7caa38861ddcd27ee7cf38fb
Krunal Gupta <guptakrunal02@gmail.com>
2024-05-13 23:11:26 +0530

    Buiding the search page

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - Change the context to redux toolkit
 - Use the infinite scrolling in the trending page
 - Add the debounce on the search bar
+- In some gif there is no info on hover fix it
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { GifState } from "../Context/Context";
+import FilterGif from "../components/Filter-gif";
+import Gif from "../components/Gif";
 
 const Search = () => {
-  return <div>Search</div>;
+  const { query } = useParams();
+
+  const [searchResult, setSearchResult] = useState([]);
+
+  const { gf, filter } = GifState();
+
+  const fetchSearchResult = async () => {
+    const { data } = await gf.search(query, {
+      sort: "relevant",
+      lang: "en",
+      type: "filter",
+      limit: 20,
+    });
+    setSearchResult(data);
+  };
+
+  useEffect(() => {
+    fetchSearchResult();
+  }, [filter]);
+
+  return (
+    <div className="my-4">
+      <h2 className="text-5xl pb-3 font-extrabold">{query}</h2>
+
+      <FilterGif alignLeft={true} />
+
+      {searchResult.length > 0 ? (
+        <div className="columns-2 md:columns-3 lg:columns-4 gap-2">
+          {searchResult.map((gif) => {
+            <Gif gif={gif} key={gif.id} />;
+          })}
+        </div>
+      ) : (
+        <span>
+          No Gif found for {query}. Try searching for Stickers instead?
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default Search;
